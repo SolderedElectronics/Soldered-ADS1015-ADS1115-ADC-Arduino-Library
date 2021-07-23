435e59740128961087b15d7b854ebd1f331cbb4e
Main Push Robot <robot@example.com>
2021-07-23 07:44:30 +0000

    Committing dev changes

---
+++ src/ADS1115-SOLDERED.h
@@ -0,0 +1,19 @@
+/**
+ **************************************************
+ *
+ * @file        ADS1015-SOLDERED.h
+ * @brief       LIbrary for ADS1015-SOLDERED.h
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     @ soldered.com
+ ***************************************************/
+
+#ifndef __SENSOR__
+#define __SENSOR__
+
+#include "Arduino.h"
+#include "libs/ADS1X15/ADS1X15.h"
+
+
+#endif
